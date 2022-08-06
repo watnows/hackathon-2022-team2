1beb226b89d038d316a908049b4945ff5c9f74db
kenty99 <ec1131hk@ed.ritsumei.ac.jp>
2022-08-06 21:50:44 +0900

    配列できた

--- src/App.js
+++ src/App.js
@@ -3,11 +3,17 @@ import React, { useState } from 'react';
 const  App = () => {
     const[text,setText] = useState("")
 
-    const[addtext,setAddText] = useState("");
+     const[addtext,setAddText] = useState([]);
 
-    const onClickAddText = () =>{
-        setAddText(text);
-        setText("");
+    // const onClickAddText = () =>{
+    //     setAddTexts(text);
+    //     setText("");
+    // }
+
+    const [addtexts, setTexts] = useState([]);
+
+    const addTexts = () => {
+        setTexts([...addtexts, text])
     }
 
     return (
@@ -17,11 +23,9 @@ const  App = () => {
               onChange={(Event) => setText(Event.target.value)}
               />
 
-            <button onClick={onClickAddText}>与える</button>
+            <button onClick={addTexts}>与える</button>
 
-              <p>{text}</p>
-
-              <p>今までの食べ物:{addtext}</p>
+              <p>今までの食べ物:{addtexts}</p>
         </div>
     );
 };
