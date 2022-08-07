f016a2b2dd80359444db72bed0e762e93dbe5ae3
mercy34mercy <is0558fs@ed.ritsumei.ac.jp>
2022-08-07 22:03:33 +0900

    pc調子悪すぎ案件

--- src/App.js
+++ src/App.js
@@ -1,47 +1,21 @@
 import React, { useState } from 'react';
- import pic from "./イラスト11.png"
- import "./App.css"
+import pic from "./イラスト11.png"
+import "./App.css"
 
- const Header = () =>{
-    return(
+const Header = () => {
+    return (
         <h3>ぼくまさし</h3>
     )
- }
-
- const Body = () =>{
-    // keep data here!!
-    return(
-        <div>
-            <Display/>
-            <Data/>
-        </div>
-    )
- }
-
- const Display = () =>{
-    return(
-        <div>
-            <img className={'size1'} src={pic} alt="picture"/>
-        </div>
-    )
- }
-
- const Data = () =>{
-    return(
-        <div>
-
-        </div>
-    )
- }
-
-const  App = () => {
-
-    const[text,setText] = useState("")
-    const [addtexts, setTexts] = useState([]);
-    const[sumcalorie,setsumcalorie] = useState(0)
+}
 
 
+//foodstring 入力した食べ物
+const Body = (foodstring) => {
     const url = "https://dry-temple-23156.herokuapp.com/calorie"
+    const [sumcalorie, setsumcalorie] = useState(0)
+    const [text, setText] = useState("")
+    const [addtexts, setTexts] = useState([]);
+
     const getText = () => {
         const requestOptions = {
             method: 'POST',
@@ -58,50 +32,91 @@ const  App = () => {
                 setTexts([[text, body["calorie"]], ...addtexts])
                 setsumcalorie(sumcalorie + parseInt(body["calorie"]))
                 // const responsejson: restaurantinfo[] = body["data"]
-
             }
             catch (err) {
-
             } finally {
-
             }
         })();
+    }
 
+    // keep data here!!
+    return (
+        <div>
+            <Display handlegettext={getText} sumcalorie={sumcalorie} text={text} />
+            <Data settext={setText} />
+        </div>
+    )
+}
 
-    };
+const Display = () => {
+    return (
+        <div>
+            <img className={'size1'} src={pic} alt="syasin" />
+        </div>
+    )
+}
 
-
-    const Pressenter = (e) =>{
-        if(e.key === 'Enter'){
-            getText()
-            setText("")
-        
+const Data = (props) => {
+    const Pressenter = (e) => {
+        if (e.key === 'Enter') {
+            props.setText("")
         }
     }
 
+    const [textarray, settextarray] = React.useState(props.addtexts)
+
+    if (textarray.length === 0) {
+        return (
+            <div className="App">
+                <input
+                    value={props.text}
+                    onChange={(Event) => props.setText(Event.target.value)}
+                    onKeyPress={Pressenter}
+                />
+                <button onClick={props.handlegettext}>与える</button>
+
+                <p>摂取カロリー{props.sumcalorie}kcal</p>
+
+                <p>今までの食べ物:</p>
+
+                <ul>
+                    {textarray.map((food, i) => <li key={i}>{food}</li>)}
+                </ul>
+                {/* <img className={'size'+ sumcalorie} src={pic} alt="picture"/> */}
+            </div>
+        )
+    } else {
+        return (
+            <div className="App">
+                <input
+                    value={props.text}
+                    onChange={(Event) => props.setText(Event.target.value)}
+                    onKeyPress={Pressenter}
+
+                />
+
+                <button onClick={props.handlegettext}>与える</button>
+
+                <p>摂取カロリー{props.sumcalorie}kcal</p>
+
+                <p>今までの食べ物:</p>
+
+                {/* <ul>
+                    {props.addtexts.map((food, i) => <li key={i}>{food}</li>)}
+                </ul> */}
+                {/* <img className={'size'+ sumcalorie} src={pic} alt="picture"/> */}
+            </div>
+        )
+    }
+}
+
+const App = () => {
     return (
-        <div className="App">
-            <Header/>
-            <Body/>
-            <input
-              value={text}
-              onChange={(Event) => setText(Event.target.value)}
-              onKeyPress={Pressenter}
-
-              />
-
-            <button onClick={getText}>与える</button>
-
-                <p>摂取カロリー{sumcalorie}kcal</p>
-
-              <p>今までの食べ物:</p>
-
-              <ul>
-                {addtexts.map((food,i)=> <li key={i}>{food}</li>)}
-              </ul>
-              {/* <img className={'size'+ sumcalorie} src={pic} alt="picture"/> */}
+        <div>
+            <Header />
+            <Body />
         </div>
     );
 };
- 
+
 export default App;
