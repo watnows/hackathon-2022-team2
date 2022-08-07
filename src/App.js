56db703be0ef08acb372b1940f656fb51540dd24
mercy34mercy <etukobamasatyan@gmail.com>
2022-08-07 22:46:32 +0900

    mercyがやりました

--- src/App.js
+++ src/App.js
@@ -9,28 +9,47 @@ const Header = () => {
 }
 
 
-//foodstring 入力した食べ物
-const Body = (foodstring) => {
-    const url = "https://dry-temple-23156.herokuapp.com/calorie"
+const Body = () => {
     const [sumcalorie, setsumcalorie] = useState(0)
+    // keep data here!!
+    return (
+        <div>
+            <Display sumcalorie={ sumcalorie } />
+            <Data sumcalorie={sumcalorie} setsumcalorie={ setsumcalorie } />
+        </div>
+    )
+}
+
+const Display = (props) => {
+    return (
+        <div>
+            <img className={'size' + Math.round(props.sumcalorie/10)} src={pic} alt="syasin" />
+        </div>
+    )
+}
+
+const Data = (props) => {
+    const url = "https://dry-temple-23156.herokuapp.com/calorie"
+   
     const [text, setText] = useState("")
-    const [addtexts, setTexts] = useState([]);
+    const [addtexts, setTexts] = useState([""]);
+
+    const requestOptions = {
+        method: 'POST',
+        mode: 'cors',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ food: text })
+    };
 
     const getText = () => {
-        const requestOptions = {
-            method: 'POST',
-            mode: 'cors',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ food: text })
-        };
-
         (async () => {
             try {
+                console.log("いれたご飯", text)
                 const response = await fetch(url, requestOptions);
                 const body = await response.json();
                 console.log(body["calorie"])
                 setTexts([[text, body["calorie"]], ...addtexts])
-                setsumcalorie(sumcalorie + parseInt(body["calorie"]))
+                props.setsumcalorie(props.sumcalorie + parseInt(body["calorie"]))
                 // const responsejson: restaurantinfo[] = body["data"]
             }
             catch (err) {
@@ -38,76 +57,36 @@ const Body = (foodstring) => {
             }
         })();
     }
-
-    // keep data here!!
-    return (
-        <div>
-            <Display handlegettext={getText} sumcalorie={sumcalorie} text={text} />
-            <Data settext={setText} />
-        </div>
-    )
-}
-
-const Display = () => {
-    return (
-        <div>
-            <img className={'size1'} src={pic} alt="syasin" />
-        </div>
-    )
-}
-
-const Data = (props) => {
     const Pressenter = (e) => {
         if (e.key === 'Enter') {
-            props.setText("")
+            gotoheroku()
         }
     }
-
-    const [textarray, settextarray] = React.useState(props.addtexts)
-
-    if (textarray.length === 0) {
-        return (
-            <div className="App">
-                <input
-                    value={props.text}
-                    onChange={(Event) => props.setText(Event.target.value)}
-                    onKeyPress={Pressenter}
-                />
-                <button onClick={props.handlegettext}>与える</button>
-
-                <p>摂取カロリー{props.sumcalorie}kcal</p>
-
-                <p>今までの食べ物:</p>
-
-                <ul>
-                    {textarray.map((food, i) => <li key={i}>{food}</li>)}
-                </ul>
-                {/* <img className={'size'+ sumcalorie} src={pic} alt="picture"/> */}
-            </div>
-        )
-    } else {
-        return (
-            <div className="App">
-                <input
-                    value={props.text}
-                    onChange={(Event) => props.setText(Event.target.value)}
-                    onKeyPress={Pressenter}
-
-                />
-
-                <button onClick={props.handlegettext}>与える</button>
-
-                <p>摂取カロリー{props.sumcalorie}kcal</p>
-
-                <p>今までの食べ物:</p>
-
-                {/* <ul>
-                    {props.addtexts.map((food, i) => <li key={i}>{food}</li>)}
-                </ul> */}
-                {/* <img className={'size'+ sumcalorie} src={pic} alt="picture"/> */}
-            </div>
-        )
+    const gotoheroku = () => {
+        setText("")
+        getText()
     }
+
+    return (
+        <div className="App">
+            <input
+                value={text}
+                onChange={(Event) => setText(Event.target.value)}
+                onKeyPress={Pressenter}
+            />
+            <button onClick={gotoheroku}>与える</button>
+
+            <p>摂取カロリー{props.sumcalorie}kcal</p>
+
+            <p>今までの食べ物:</p>
+
+            <ul>
+                {addtexts.map((food, i) => <li key={i}>{food}</li>)}
+            </ul>
+            {/* <img className={'size'+ sumcalorie} src={pic} alt="picture"/> */}
+        </div>
+    )
+
 }
 
 const App = () => {
